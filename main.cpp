78dc2b167ac42df4b59b9a5ea7d7205a611e4225
Maniform <maniform.development@gmail.com>
2025-04-03 19:11:17 -0400

    Fixed main.cpp declarations

--- main.cpp
+++ main.cpp
@@ -1,41 +1,36 @@
 #define OLC_PGE_APPLICATION
 #include <C8EnginePE.h>
 
-#include <print>
+#include <iostream>
 
-//#define TEST_ROM "C:/Users/nvill/OneDrive/Documents/Projects/Cpp/C8Engine/roms/chip8-roms/demos/Maze (alt) [David Winter, 199x].ch8"
-//#define TEST_ROM "C:/Users/nvill/OneDrive/Documents/Projects/Cpp/C8Engine/roms/chip8-roms/demos/Maze [David Winter, 199x].ch8"
-//#define TEST_ROM "C:/Users/nvill/OneDrive/Documents/Projects/Cpp/C8Engine/roms/chip8-roms/demos/Particle Demo [zeroZshadow, 2008].ch8"
-//#define TEST_ROM "C:/Users/nvill/OneDrive/Documents/Projects/Cpp/C8Engine/roms/chip8-roms/demos/Trip8 Demo (2008) [Revival Studios].ch8"
-//#define TEST_ROM "C:/Users/nvill/OneDrive/Documents/Projects/Cpp/C8Engine/roms/chip8-roms/demos/Zero Demo [zeroZshadow, 2007].ch8"
+#define TEST_ROM "demos/Maze (alt) [David Winter, 199x].ch8"
+//#define TEST_ROM "demos/Maze [David Winter, 199x].ch8"
+//#define TEST_ROM "demos/Particle Demo [zeroZshadow, 2008].ch8"
+//#define TEST_ROM "demos/Trip8 Demo (2008) [Revival Studios].ch8"
+//#define TEST_ROM "demos/Zero Demo [zeroZshadow, 2007].ch8"
 
-//#define TEST_ROM "C:/Users/nvill/OneDrive/Documents/Projects/Cpp/C8Engine/roms/chip8-roms/games/15 Puzzle [Roger Ivie] (alt).ch8"
-//#define TEST_ROM "C:/Users/nvill/OneDrive/Documents/Projects/Cpp/C8Engine/roms/chip8-roms/games/15 Puzzle [Roger Ivie].ch8"
-//#define TEST_ROM "C:/Users/nvill/OneDrive/Documents/Projects/Cpp/C8Engine/roms/chip8-roms/games/Addition Problems [Paul C. Moews].ch8"
-//#define TEST_ROM "C:/Users/nvill/OneDrive/Documents/Projects/Cpp/C8Engine/roms/chip8-roms/games/Tic-Tac-Toe [David Winter].ch8"
-
-//#define TEST_ROM "/Users/noel/Documents/Projets/YAC8I/3rdParty/chip8-roms/games/Addition Problems [Paul C. Moews].ch8"
-//#define TEST_ROM "/Users/noel/Documents/Projets/YAC8I/3rdParty/chip8-roms/demos/Maze (alt) [David Winter, 199x].ch8"
-//#define TEST_ROM "/Users/noel/Documents/Projets/YAC8I/3rdParty/chip8-roms/demos/Trip8 Demo (2008) [Revival Studios].ch8"
-#define TEST_ROM "/Users/noel/Documents/Projets/YAC8I/3rdParty/chip8-roms/demos/Particle Demo [zeroZshadow, 2008].ch8"
+//#define TEST_ROM "games/15 Puzzle [Roger Ivie] (alt).ch8"
+//#define TEST_ROM "games/15 Puzzle [Roger Ivie].ch8"
+//#define TEST_ROM "games/Addition Problems [Paul C. Moews].ch8"
+//#define TEST_ROM "games/Tic-Tac-Toe [David Winter].ch8"
 
 int main(int argc, char** argv)
 {
-    try
-    {
+	try
+	{
 		C8EnginePE demo;
 		
-		const string rom = argc > 1 ? argv[1] : TEST_ROM;
+		const string rom = argc > 1 ? argv[1] : string(ROMS_FOLDER) + TEST_ROM;
 		demo.loadRomFromFile(rom.c_str());
 		
-        while(demo.update())
-        {
-        }
-    }
-    catch(const std::exception& e)
-    {
-        printf("Error : %s\n", e.what());
-    }
+		while(demo.update())
+		{
+		}
+	}
+	catch(const std::exception& e)
+	{
+		cerr << "Error : " << e.what() << endl;
+	}
 
 	return 0;
 }
